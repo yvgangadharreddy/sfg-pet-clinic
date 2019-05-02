6741b0865723c857def2c31c8434b4c1df3bd3aa
Gangadhar <gangadhar.reddy729@gmail.com>
2019-05-02 12:19:03 +0530

    GR - spring pet clinic initial commit

---
+++ src/main/java/com/springframework/spring5petclinic/Spring5PetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.spring5petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5PetClinicApplication.class, args);
+    }
+
+}
